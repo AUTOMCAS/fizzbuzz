09e3cb5a670673ca746a4b9ce2dcf43e808cd66f
Chris <caston88@gmail.com>
2022-09-07 12:31:16 +0100

    Add to fizzbuzz method to return fizzbuzz if number is multiple of 5 and 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,9 @@
 def fizzbuzz(numbers)
     fizz = numbers % 3 == 0
     buzz = numbers % 5 == 0
-    if fizz
+    if fizz && buzz
+        "fizzbuzz"
+    elsif fizz
         "fizz"
     elsif buzz
         "buzz"
